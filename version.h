ae5e4aecb6c309f1214adbab31ac273a8ab523a7
Cameron Gutman <aicommander@gmail.com>
2020-02-21 17:38:44 -0800

    Reserve version 2.3.2 for MIHT

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define VER_VERSION                 2,3,1,0
-#define VER_VERSION_STR             "2.3.1.0"
+#define VER_VERSION                 2,3,2,0
+#define VER_VERSION_STR             "2.3.2.0"
 
 #define VER_COMPANYNAME_STR         "Moonlight Game Streaming Project"
 #define VER_PRODUCTNAME_STR         "Moonlight Internet Hosting Tool"
