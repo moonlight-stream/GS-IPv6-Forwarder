4c94ecfce171f123cf6ebf787005edb92f9f21c4
Cameron Gutman <aicommander@gmail.com>
2023-08-25 23:42:01 -0500

    Version 2.6.0

--- version.h
+++ version.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define VER_VERSION                 2,5,4,0
-#define VER_VERSION_STR             "2.5.4.0"
+#define VER_VERSION                 2,6,0,0
+#define VER_VERSION_STR             "2.6.0.0"
 
 #define VER_COMPANYNAME_STR         "Moonlight Game Streaming Project"
 #define VER_PRODUCTNAME_STR         "Moonlight Internet Hosting Tool"
